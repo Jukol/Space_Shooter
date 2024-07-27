22998086b3912eadfedb75f36b326023c7ca459a
Oleh Yukhymenko <yukhymster@gmail.com>
2024-07-27 13:17:06 +0300

    Some UI arts added

--- Assets/Scripts/UI/LevelSelector.cs
+++ Assets/Scripts/UI/LevelSelector.cs
@@ -1,6 +1,8 @@
+using System;
 using EnemyScripts;
 using Infrastructure.GameLaunch;
 using Infrastructure.States;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,18 +12,39 @@ namespace UI
     public class LevelSelector : MonoBehaviour
     {
         [SerializeField] private Button closeButton;
-        
+        [SerializeField] private Button levelButtonPrefab;
+        [SerializeField] private Transform levelButtonHolder;
+
         private GameInitializer _gameInitializer;
 
         private int _completedSpawnManagerId;
         private GameStateMachine _gameStateMachine;
         private SpawnManager _spawnManager;
+        private bool _buttonsGenerated;
 
         public void Init(GameInitializer gameInitializer, GameStateMachine gameStateMachine, SpawnManager spawnManager)
         {
             _gameInitializer = gameInitializer;
             _gameStateMachine = gameStateMachine;
             _spawnManager = spawnManager;
+
+            if (!_buttonsGenerated)
+            {
+                GenerateLevelButtons();
+            }
+        }
+
+        private void GenerateLevelButtons()
+        {
+            for (int i = 0; i < SceneManager.sceneCountInBuildSettings - 1; i++)
+            {
+                Button levelButton = Instantiate(levelButtonPrefab, levelButtonHolder);
+                levelButton.GetComponentInChildren<TMP_Text>().text = (i + 1).ToString();
+                int level = i + 1;
+                levelButton.onClick.AddListener(() => SelectLevel(level));
+            }
+            
+            _buttonsGenerated = true;
         }
 
         private void OnEnable()
