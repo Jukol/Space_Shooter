1da25a749a511b2f3b21afee3666c144a3ee72fe
Oleh Yukhymenko <yukhymster@gmail.com>
2024-08-06 14:08:10 +0300

    Drop system implemented

---
+++ Assets/Scripts/Drops/Drop.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Drops
+{
+    public abstract class Drop : MonoBehaviour
+    {
+        public float Speed { get => speed; set => speed = value;}
+
+        [SerializeField] private float speed;
+
+        public bool startMoving;
+
+        private void Awake()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (startMoving)
+            {
+                MoveDown();
+            }
+        }
+
+        private void MoveDown()
+        {
+            transform.position += Vector3.down * Time.deltaTime * speed;
+        }
+
+        public abstract void OnTriggerEnter2D(Collider2D other);
+    }
+}
---
+++ Assets/Scripts/Drops/UpgradeDrop.cs
@@ -0,0 +1,17 @@
+﻿using PlayerScripts;
+using UnityEngine;
+
+namespace Drops
+{
+    public class UpgradeDrop : Drop
+    {
+        public override void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                other.GetComponent<Player>().Upgrade();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
