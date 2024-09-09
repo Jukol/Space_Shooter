041d44049d8dc30dabc58a0dafc7fab41737bd89
Oleh Yukhymenko <yukhymster@gmail.com>
2024-09-09 12:33:25 +0300

    Level 3 enemy placeholders edited

---
+++ Assets/Scripts/ShowInScene.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ShowInScene2D : MonoBehaviour
+{
+    public Color gizmoColor = Color.green;
+    public float gizmoRadius = 0.2f;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        DrawWireCircle(transform.position, gizmoRadius);
+    }
+
+    void DrawWireCircle(Vector3 position, float radius)
+    {
+        int segments = 32;  
+        float angleStep = 360f / segments;
+
+        Vector3 previousPoint = position + new Vector3(Mathf.Cos(0), Mathf.Sin(0)) * radius;
+
+        for (int i = 1; i <= segments; i++)
+        {
+            float angle = Mathf.Deg2Rad * angleStep * i;
+            Vector3 nextPoint = position + new Vector3(Mathf.Cos(angle), Mathf.Sin(angle)) * radius;
+            Gizmos.DrawLine(previousPoint, nextPoint);
+            previousPoint = nextPoint;
+        }
+    }
+}
